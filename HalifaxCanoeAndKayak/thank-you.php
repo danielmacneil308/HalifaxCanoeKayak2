e5141ce22c52d6abb5b6954646065b0167ab6178
Daniel MacNeil <76018086+danielmacneil308@users.noreply.github.com>
2022-11-21 19:50:48 -0400

    Update thank-you.php

--- HalifaxCanoeAndKayak/thank-you.php
+++ HalifaxCanoeAndKayak/thank-you.php
@@ -10,7 +10,10 @@ $trip_date = $_GET['tripdate'];
 
 ?>
 <html>
+    <head><link rel="stylesheet" href="./css/main.css" type="text/css"></head>
     <h1>Thank You</h1><hr>
     <p>Thank you <?=$email?></p>
     <p>We will contact you about the <?=$location?> trip on <?=$trip_date?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="./js/main.js"></script>
 </html>
